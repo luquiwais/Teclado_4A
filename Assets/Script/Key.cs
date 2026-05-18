2c5d1a4ac3d54cab8628b68d3052371f030e6f2b
luquiwais <luquiwais@gmail.com>
2026-05-18 12:04:33 -0300

    terminado

--- Assets/Script/Key.cs
+++ Assets/Script/Key.cs
@@ -4,7 +4,26 @@ using UnityEngine;
 
 public class Key : MonoBehaviour
 {
+    public float cantidadAlBajar = 0.2f; 
+    
+    private Vector3 posicionOriginal;
+    private Vector3 posicionPresionada;
+    void Start()
+    {
+        // Guardamos la posición exacta donde empezó la tecla
+        posicionOriginal = transform.position;
+        
+        // Calculamos la posición hacia abajo
+        posicionPresionada = posicionOriginal + new Vector3(0, -cantidadAlBajar, 0);
+    }
     void OnMouseDown(){
         Debug.Log("Click");
+        transform.position=posicionPresionada;
+    }
+
+    void OnMouseUp()
+    {
+        Debug.Log("se solto la tecla");
+        transform.position=posicionOriginal;
     }
 }
---
+++ Assets/Script/KeyboardManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyboardManager : MonoBehaviour
+{
+    public Key[] Keys;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Keys = GameObject.FindObjectsOfType<Key>();
+        if (Keys.Length > 0)
+        {
+          int indiceAzar = Random.Range(0, Keys.Length);
+          Destroy(Keys[indiceAzar].gameObject);
+          Debug.Log("Se destruyó la tecla en el índice: " + indiceAzar);
+        }else
+        {
+            Debug.LogWarning("No se encontraron teclas en la escena.");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
